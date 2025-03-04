b9f52b9cdb59cec34f6a7d44f793930f6c27d35c
Sean Murphy <sean@iamseanmurphy.com>
2025-03-04 11:16:42 +0000

    1.4.2

--- sureforms/assets/build/settings.asset.php
+++ sureforms/assets/build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '7bd5b022883dd5f64000');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0a12037cebfe3c80ad1d');
--- sureforms/assets/build/templatePicker.asset.php
+++ sureforms/assets/build/templatePicker.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '20b486c3f8049cf0e202');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6f70496e6263cc890f04');
