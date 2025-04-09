e5e4d1d0290a14c226958a64f6fd825e23ca186f
Sean Murphy <sean@iamseanmurphy.com>
2025-04-09 12:19:15 +0000

    1.5.0

--- sureforms/assets/build/formSubmit.asset.php
+++ sureforms/assets/build/formSubmit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => 'e1d7ef1587972f6051e1');
+<?php return array('dependencies' => array('wp-hooks', 'wp-i18n'), 'version' => '46fd264f2baeb66a81f5');
