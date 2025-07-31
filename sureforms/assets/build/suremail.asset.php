4450e6dc123885cea614f31bdcb9d1c0cccd5865
Sean Murphy <sean@iamseanmurphy.com>
2025-07-31 12:20:17 +0000

    1.9.1

--- sureforms/assets/build/suremail.asset.php
+++ sureforms/assets/build/suremail.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '4ed9d390d9bbc6f46ec8');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '301bf0f303a184932fe9');
